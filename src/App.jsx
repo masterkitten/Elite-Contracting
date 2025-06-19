d3522b31431fc40a96376f164b58d4302f27491a
Akib Islam <akibislam@Akibs-MacBook-Air.local>
2025-06-19 13:24:14 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <main style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>Elite Contracting</h1>
+      <p>NYC’s Trusted Experts in Full-Service Construction & Renovation.</p>
+    </main>
+  )
+}
