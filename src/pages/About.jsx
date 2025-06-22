3723b797586a17bdd409692f0c1ca0468a0dd21f
masterkitten <akibislam1201@gmail.com>
2025-06-22 17:56:20 -0400

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,1 +1,11 @@
+function About() {
+  return (
+    <div className="page">
+      <h2>About Us</h2>
+      <p>We are a trusted NYC-based contracting company serving all five boroughs.</p>
+    </div>
+  );
+}
+
+export default About;
 
