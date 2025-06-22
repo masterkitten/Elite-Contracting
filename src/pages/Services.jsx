148a081988737c0a70ed2b84c837da57333ff9cb
masterkitten <akibislam1201@gmail.com>
2025-06-22 17:56:36 -0400

    Update Services.jsx

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,1 +1,15 @@
+function Services() {
+  return (
+    <div className="page">
+      <h2>Our Services</h2>
+      <ul>
+        <li>Construction</li>
+        <li>Renovation</li>
+        <li>Restoration</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Services;
 
