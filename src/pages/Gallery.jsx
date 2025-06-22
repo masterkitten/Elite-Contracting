03827ad91c510629331de72d10093301f49f2f47
masterkitten <akibislam1201@gmail.com>
2025-06-22 17:56:54 -0400

    Update Gallery.jsx

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -1,1 +1,11 @@
+function Gallery() {
+  return (
+    <div className="page">
+      <h2>Project Gallery</h2>
+      <p>Take a look at some of our past work.</p>
+    </div>
+  );
+}
+
+export default Gallery;
 
